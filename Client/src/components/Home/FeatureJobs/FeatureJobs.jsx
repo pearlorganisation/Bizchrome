0d5d9445c86c4eb6e99d1c3620d198ada9d9d5f9
Abhishek Bahuguna <abhishek@pearlorganisation.com>
2024-02-24 15:41:20 +0530

    Added changes on home page ui

--- Client/src/components/Home/FeatureJobs/FeatureJobs.jsx
+++ Client/src/components/Home/FeatureJobs/FeatureJobs.jsx
@@ -1,32 +1,129 @@
 import { IoIosArrowForward } from "react-icons/io";
 import Marquee from "react-fast-marquee";
+import { FaHome } from "react-icons/fa";
 const FeatureJobs = () => {
+  const jobsOpening = [
+    {
+      title: "Work-From-Home Jobs",
+      desc: "82 open positions",
+      icon: <FaHome size={30} />,
+    },
+    {
+      title: "Freelancing Jobs",
+      desc: "21 open positions",
+      icon: <FaHome size={30} />,
+
+    },
+    {
+      title: "Accounting/Finance",
+      desc: "43 open positions",
+      icon: <FaHome size={30} />,
+
+    },
+    {
+      title: "IT Jobs",
+      desc: "145 open positions",
+      icon: <FaHome size={30} />,
+
+    },
+    {
+      title: "Work-From-Home Jobs",
+      desc: "82 open positions",
+      icon: <FaHome size={30} />,
+
+    },
+    {
+      title: "Freelancing Jobs",
+      desc: "21 open positions",
+      icon: <FaHome size={30} />,
+
+    },
+    {
+      title: "Accounting/Finance",
+      desc: "43 open positions",
+      icon: <FaHome size={30} />,
+
+    },
+    {
+      title: "IT Jobs",
+      desc: "145 open positions",
+      icon: <FaHome size={30} />,
+
+    },
+    {
+      title: "Work-From-Home Jobs",
+      desc: "82 open positions",
+      icon: <FaHome size={30} />,
+
+    },
+    {
+      title: "Freelancing Jobs",
+      desc: "21 open positions",
+      icon: <FaHome size={30} />,
+
+    },
+    {
+      title: "Accounting/Finance",
+      desc: "43 open positions",
+      icon: <FaHome size={30} />,
+
+    },
+    {
+      title: "IT Jobs",
+      desc: "145 open positions",
+      icon: <FaHome size={30} />,
+
+    },
+    {
+      title: "Work-From-Home Jobs",
+      desc: "82 open positions",
+      icon: <FaHome size={30} />,
+
+    },
+    {
+      title: "Freelancing Jobs",
+      desc: "21 open positions",
+      icon: <FaHome size={30} />,
+
+    },
+    {
+      title: "Accounting/Finance",
+      desc: "43 open positions",
+      icon: <FaHome size={30} />,
+
+    },
+    {
+      title: "IT Jobs",
+      desc: "145 open positions",
+      icon: <FaHome size={30} />,
+
+    },
+  ];
   return (
-    <>
+    <div className="py-12">
       <div className="text-center ">
-        <h1 className="text-lg text-xl md:text-3xl font-semibold">
+        <h1 className="text-lg md:text-3xl font-semibold">
           Trending job roles on Bizchrome
         </h1>
       </div>
       <div className=" py-6 w-screen">
         <Marquee pauseOnHover speed={100}>
-          {Array(20)
-            .fill(true)
+          {jobsOpening
             .map((item, indx) => (
-              <div className="mx-5 cursor-pointer">
+              <div className="mx-5 cursor-pointer group">
                 <div className="flex justify-start items-center border-2 rounded-lg max-w-[280px] hover:border-2 transition-all  hover:border-teal-400">
                   <div className="p-5">
-                    <div>
-                      <img src="https://storage.googleapis.com/mumbai_apnatime_prod/department_icons/32.svg" />
+                    <div className="text-gray-300 group-hover:text-teal-400">
+                      {item.icon}
                     </div>
                   </div>
 
                   <div className="flex flex-col ">
                     <div>
-                      <h1>Tool and die maker</h1>
+                      <h1 className="line-clamp-1">{item.title}</h1>
                     </div>
                     <div>
-                      <p>52 opening</p>
+                      <p>{item.desc}</p>
                     </div>
                   </div>
                   <div>
@@ -42,23 +139,22 @@ const FeatureJobs = () => {
 
       <div className=" py-6 w-screen">
         <Marquee direction="right" pauseOnHover speed={100}>
-          {Array(20)
-            .fill(true)
+          {jobsOpening
             .map((item, indx) => (
-              <div className="mx-5 cursor-pointer">
+              <div className="mx-5 cursor-pointer group">
                 <div className="flex justify-start items-center border-2 rounded-lg max-w-[280px] hover:border-2 transition-all  hover:border-teal-400">
                   <div className="p-5">
-                    <div>
-                      <img src="https://storage.googleapis.com/mumbai_apnatime_prod/department_icons/32.svg" />
+                    <div className="text-gray-300 group-hover:text-teal-400">
+                      {item.icon}
                     </div>
                   </div>
 
                   <div className="flex flex-col ">
                     <div>
-                      <h1>Tool and die maker</h1>
+                      <h1 className="line-clamp-1">{item.title}</h1>
                     </div>
                     <div>
-                      <p>52 opening</p>
+                      <p>{item.desc}</p>
                     </div>
                   </div>
                   <div>
@@ -71,7 +167,7 @@ const FeatureJobs = () => {
             ))}
         </Marquee>
       </div>
-    </>
+    </div>
   );
 };
 
